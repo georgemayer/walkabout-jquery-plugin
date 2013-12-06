ab99eaf97ce9e8bcfecff682bf09661e7fc649e2
georgemayer <mayer.georgep@gmail.com>
2013-12-06 18:54:34 -0500

    extended support
    Support for fixed navs
    support for titles of hints

--- walkabout.js
+++ walkabout.js
@@ -6,7 +6,8 @@
         // set the defaults
         var defaults = {
             autostart: false, // autostart will start the tutorial on page load
-            begin: "" // begin accepts a string of the CLASS of an element that will trigger the tutorial on click 
+            begin: "", // begin accepts a string of the CLASS of an element that will trigger the tutorial on click 
+            stickNav: false//accept a boolean. Is the nav fixed or not?
         }; 
         
         // allow the user to add her own options
@@ -30,95 +31,88 @@
                 // runs the tutorial, tutorial begins un-initialized
                 // begins to data-tut-step one
                 // create a list of all of the elements
-                newDiv = $("<div>").addClass("hint");
-                currentStep = this.current;
-                step = $("[data-tut-step="+currentStep+"]");
-                this.showStep(step);
+                if($(".hint").length < 1) {
+                    newDiv = $("<div>").addClass("hint");
+                    currentStep = this.current;
+                    step = $("[data-tut-step="+currentStep+"]");
+                    this.showStep(step);
+                }    
             },
             showStep: function(step) {
                 // shows an actual tutorial step  
                 // create the hint
+                title = step.attr("data-tut-title");
+                titleDiv = $("<div>");
+                titleDiv.text(title);
+                titleDiv.addClass("hint-title");
+                newDiv.append(titleDiv);
                 content = step.attr("data-tut-content");
                 para = $("<p>");
                 para.text(content);
                 newDiv.append(para);
                 closeHint = $("<div>");
-                closeHint.text("x");
+                closeHint.html("<i class='icon-remove'></i>");
                 closeHint.click(function(){
                     tutorial.closeStep();
                 });
-                closeHint.addClass('close');
+                closeHint.addClass('close-hint');
                 newDiv.append(closeHint);
                 
                 // create the next button
                 // check if the current step is the last step
                 if(this.current !== this.totalSteps()) {  
                     nextButton = $("<button>");
-                    nextButton.html("Next");
+                    nextButton.html("next <i class='icon-chevron-right'></i>");
                     nextButton.addClass("next");
                     nextButton.click(function(e){
                         e.preventDefault();
                         tutorial.next();  
                     });
                     newDiv.append(nextButton);
+                } else {
+                    closeButton = $("<button>");
+                    closeButton.html("You're done!");
+                    closeButton.addClass("end-tut");
+                    closeButton.click(function(e){
+                        e.preventDefault();
+                        tutorial.closeStep();
+                        tutorial.current = 1;
+                        console.log(tutorial.current);
+                    });
+                    newDiv.append(closeButton);
+                    
                 }
                 // create the previous button
                 // check if the current step is the first step
                 if(this.current !== 1) {
                     prevButton = $("<button>");
-                    prevButton.html("Prev");
+                    prevButton.html("<i class='icon-chevron-left'></i> previous");
                     prevButton.addClass("prev");
                     prevButton.click(function(e){
                         e.preventDefault();
                         tutorial.previous();  
                     });
                     newDiv.append(prevButton);
-                }   
+                }  
                 
-                // position the hint
-                pos = step.offset(); 
-                wHeight = $(document).height();
-                wWidth = $(window).width();
-                hintHeight = newDiv.height();
-                hintWidth = newDiv.width();
-                fromBottom = wHeight - pos.top;
-                fromRight = wWidth - pos.left;
-                console.log(fromRight);
-                //newDiv.css("left",pos.left);
-        
-                if(fromRight < hintWidth) {
-                    newDiv.addClass("from-right");
-                    if(newDiv.hasClass("from-left")) newDiv.removeClass("from-left");
-                    newDiv.css("left",pos.left - newDiv.width());
-                } else {
-                    newDiv.css("left",pos.left);
-                    newDiv.addClass("from-left");
-                    if(newDiv.hasClass("from-right")) newDiv.removeClass("from-right");
-                    newDiv.css("left",pos.left);
-                }     
+                var position = positionTheHint(newDiv, step);
                 
-                // if the step is at the bottom of the screen, put the hint above it
-                if(fromBottom < hintHeight) {
-                    bottomOffset = fromBottom + step.height();
-                    newDiv.addClass("from-bottom");
-                    if(newDiv.hasClass("from-top")) newDiv.removeClass("from-top");
-                    newDiv.css("bottom",bottomOffset+"px");   
-                } else {
-                    if(newDiv.hasClass("from-bottom")) newDiv.removeClass("from-bottom");
-                    newDiv.addClass("from-top");
-                    newDiv.css("bottom","auto");  
-                }
-                // if the step is on an input, put it after the element, rather than as a child of it. 
-                if(step.is('input')) {       
-                    step.after(newDiv);
-                } else {
-                    step.append(newDiv);
-                } 
+                newDiv.css({
+                    "left": position.left,
+                    "top": position.top
+                });
+                newDiv.removeClass();
+                newDiv.addClass("hint");
+                newDiv.addClass(position.horizHintClass);
+                newDiv.addClass(position.vertHintClass);
+
+                $("body").append(newDiv);
+                newDiv.draggable();
           
             },
             // hide the hint
             closeStep: function() {
-                $(".hint").hide();  
+                $(".hint").remove();  
             }, 
             
             // go to the next hint
@@ -128,11 +122,8 @@
                 console.log(nextStep);
                 step = $("[data-tut-step="+nextStep+"]");
                 scrollTo(step);
-                if(step.length > 0) {
-                    this.showStep(step); 
-                } else {
-                    this.closeStep();
-                }       
+                this.showStep(step); 
+      
             },
             // go to the previous hint
             previous: function() {
@@ -141,17 +132,54 @@
                 console.log(prevStep);
                 step = $("[data-tut-step="+prevStep+"]");
                 scrollTo(step);
-                if(step.length > 0) {
-                    this.showStep(step); 
-                } else {
-                    this.closeStep();
-                }
+                this.showStep(step); 
+
             }
         };
+
+        function positionTheHint(hint, step) {
+            docHeight = $(document).height();
+            windowWidth = $(window).width();
+            hintHeight = hint.outerHeight();
+            hintWidth = hint.width();
+            stepPosition = step.offset(); 
+            stepOffsetBottom = docHeight - stepPosition.top;         
+            stepOffsetRight = windowWidth - stepPosition.left;
+            stepHeight = step.height();
+            
+            position = {};
+            if(stepOffsetRight < hintWidth) {
+                position.horizHintClass = "from-right";
+                position.left = stepPosition.left - hintWidth + (step.outerWidth()/2);
+            } else {
+                position.horizHintClass = "from-left";
+                position.left = stepPosition.left + (step.outerWidth()/2) - 20; 
+            }     
+            
+            if(stepOffsetBottom < hintHeight) {
+                position.vertHintClass = "from-bottom";
+                position.top = stepPosition.top - stepHeight - hintHeight;
+            } else {
+                position.vertHintClass = "from-top";
+                position.top = stepPosition.top + stepHeight + 20;
+            }
+            
+            return position;
+        }
         
         function scrollTo(el) {
             // takes an element and visually scrolls the window to that element
+
             destination = el.offset().top;
+            windowOffset = destination - $(window).scrollTop();
+            stepHeight = el.outerHeight();
+            
+            if(options.stickNav === true) {
+                isNavChild = el.parents("nav").length;
+                if(isNavChild < 1) {
+                    if(stepHeight < 60 && destination > 60) destination -= (60+stepHeight);
+                }
+            }  
             $('html, body').animate({
                 scrollTop: destination
             }, 1000);        
